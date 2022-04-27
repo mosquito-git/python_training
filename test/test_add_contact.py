6f2c6fcc442e9dd198a3685931a42d713e83c033
Evgeny thinkpad <evg_lenovo@example.com>
2022-04-27 18:49:17 +0300

    созданы пакеты для разных зон ответственности

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-import contact
-from application import Application
+from model import contact
+from fixture.application import Application
 
 
 @pytest.fixture
