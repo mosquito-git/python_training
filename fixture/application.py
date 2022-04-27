ccc3d6125015aacb3164cf03f69b875605e7784a
Evgeny thinkpad <evg_lenovo@example.com>
2022-04-27 19:45:59 +0300

    выделен класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,20 +1,22 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from fixture.session import SessionHelper
 
 
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://127.0.0.1/addressbook/")
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "Logout").click()
+    # def logout(self):
+    #     wd = self.wd
+    #     wd.find_element(By.LINK_TEXT, "Logout").click()
 
     def return_to_group_page(self):
         wd = self.wd
@@ -43,18 +45,18 @@ class Application:
         wd = self.wd
         wd.find_element(By.LINK_TEXT, "groups").click()
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.ID, "LoginForm").click()
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+    # def login(self, username, password):
+    #     wd = self.wd
+    #     self.open_home_page()
+    #     wd.find_element(By.NAME, "user").click()
+    #     wd.find_element(By.NAME, "user").clear()
+    #     wd.find_element(By.NAME, "user").send_keys(username)
+    #     wd.find_element(By.ID, "LoginForm").click()
+    #     wd.find_element(By.NAME, "pass").click()
+    #     wd.find_element(By.NAME, "pass").click()
+    #     wd.find_element(By.NAME, "pass").clear()
+    #     wd.find_element(By.NAME, "pass").send_keys(password)
+    #     wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
     def create_contact(self, contact):
         wd = self.wd
