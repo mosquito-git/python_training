724dfe0aee4365e221eeded6562e13c27b1771e6
Evgeny thinkpad <evg_lenovo@example.com>
2022-04-29 16:16:43 +0300

    почистил от старого

--- conftest.py
+++ conftest.py
@@ -9,10 +9,8 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        # fixture.session.login(username="admin", password="secret")
     elif not fixture.is_valid():
         fixture = Application()
-        # fixture.session.login(username="admin", password="secret")
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,12 +4,9 @@ from model.group import Group
 
 
 def test_add_group1(app):
-    # app.session.login(username="admin", password="secret")
     app.group.create(Group(name="dffdfdf", header="dfdfdfd", footer="dfdfdf"))
-    # app.session.logout()
 
 
 def test_add_empty_group(app):
-    # app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    # app.session.logout()
+
