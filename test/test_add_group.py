9169375dd2fb7a787d3b19141af57f35595cb358
Evgeny thinkpad <evg_lenovo@example.com>
2022-06-02 00:08:28 +0300

    динамическая генерация тестов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-import pytest
+# import pytest
 from model.group import Group
-from data.add_group import constant as testdata
+# from data.groups import constant as testdata
 
 
 #
@@ -24,8 +24,9 @@ from data.add_group import constant as testdata
 # ]
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group1(app, group):
+# @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group1(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
