b8feaf5325eae085460440803647155181d33519
Evgeny thinkpad <evg_lenovo@example.com>
2022-06-07 09:26:08 +0300

    task 18 + task 19 (reformat files)

--- generator/group.py
+++ generator/group.py
@@ -33,9 +33,8 @@ testdata = [Group(name="", header="", footer="")] + [
 ]
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..",  f)
-# print(file)
+
 
 with open(file, "w") as out:
     jsonpickle.set_encoder_options("json", indent=2)
     out.write(jsonpickle.encode(testdata))
-    # out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,30 +1,7 @@
 # -*- coding: utf-8 -*-
-# import pytest
 from model.group import Group
-# from data.groups import constant as testdata
 
 
-#
-# def random_string(prefix, maxlen):
-#     symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
-#     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-#
-#
-# testdata = [Group(name="", header="", footer="")] + [
-#     Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-#     for i in range(5)
-# ]
-
-
-# testdata = [
-#     Group(name=name, header=header, footer=footer)
-#     for name in ["",random_string("name", 10)]
-#     for header in ["",random_string("header", 20)]
-#     for footer in ["",random_string("footer", 20)]
-# ]
-
-
-# @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group1(app, json_groups):
     group = json_groups
     old_groups = app.group.get_group_list()
@@ -33,4 +10,3 @@ def test_add_group1(app, json_groups):
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
