a1a78a19cc42ee485ba1c983b7ca2cbe32a1ffe0
Evgeny thinkpad <evg_lenovo@example.com>
2022-06-09 01:20:21 +0300

    task 21

--- test/test_contact_home_page_data.py
+++ test/test_contact_home_page_data.py
@@ -4,13 +4,11 @@ import re
 
 
 def test_data_on_home_page_and_from_db(app, db):
-    some_contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
-    contact_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
-
-    print('some_contacts_from_home_page=',some_contacts_from_home_page)
-    print('contact_from_db=', contact_from_db)
-
-    assert sorted(some_contacts_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
+    contacts_from_home_page = app.contact.get_contact_list()
+    contact_from_db = db.get_contact_list()
+    # print('some_contacts_from_home_page=',contacts_from_home_page)
+    # print('contact_from_db=', contact_from_db)
+    assert sorted(contacts_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
 
 
 
