c177e25b561f0e571913c2c3a52e6aae11452bde
Evgeny thinkpad <evg_lenovo@example.com>
2022-06-14 10:01:58 +0300

    task 20  доработка

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -23,9 +23,11 @@ def test_edit_first_contact1(app, db, check_ui):
     app.contact.return_to_home_page()
     new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[idx] = cont
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
-        old_contacts[idx] = cont
-        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+        new_contacts = app.contact.get_contact_list()
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 #
 # def test_edit_first_contact_firstname(app):
